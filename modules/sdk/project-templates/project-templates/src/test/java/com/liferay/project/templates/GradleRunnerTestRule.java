0e8fb59b1c41d116ea92446b83ef4a85a3ef3832
Lawrence Lee <lawrence.lee@liferay.com>
2019-09-27 09:47:18 -0500

    LPS-120102 Use TestRule instead of MethodRule

--- modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/GradleRunnerTestRule.java
+++ modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/GradleRunnerTestRule.java
@@ -1,21 +1,26 @@
 package com.liferay.project.templates;
 
+import java.io.File;
+
 import org.gradle.testkit.runner.GradleRunner;
-import org.junit.rules.MethodRule;
-import org.junit.runners.model.FrameworkMethod;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-public class GradleRunnerTestRule implements MethodRule{
+public class GradleRunnerTestRule implements TestRule{
 
 	public GradleRunner gradleRunner;
+	public File projectDir = new File(".");
 
 	@Override
-	public Statement apply(Statement statement, FrameworkMethod frameworkMethod, Object target) {
+	public Statement apply(Statement statement, Description description) {
 
 		return new Statement() {
 			 @Override
 	            public void evaluate() throws Throwable {
 				 	gradleRunner = GradleRunner.create();
+				 	gradleRunner.withProjectDir(projectDir);
+				 	gradleRunner.withArguments("--stop").build();
 				 try {
 					 statement.evaluate();
 
